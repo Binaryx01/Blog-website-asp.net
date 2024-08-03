8e4db29f9e02cd73d2e9a984df7bec2ad63db367
Ghost <Ghost@BINARYX01>
2024-08-03 07:55:39 +0545

    - Database setup is added - One view is added

--- wlogit/Program.cs
+++ wlogit/Program.cs
@@ -1,5 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using wlogit.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddMvc();
+var myConString = builder.Configuration.GetConnectionString("myString");
+builder.Services.AddDbContext<AppDbContext>(Options => Options.UseSqlServer(myConString));
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler();
+    app.UseHsts();
+}
+
+
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 
