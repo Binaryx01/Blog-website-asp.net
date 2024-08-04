184c3a42c968baa19d78946222feadbd529cd692
Ghost <Ghost@BINARYX01>
2024-08-04 15:20:33 +0545

    Redmore Btn works now and short permalink

--- wlogit/Controllers/HomeController.cs
+++ wlogit/Controllers/HomeController.cs
@@ -1,17 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using wlogit.Data;
+using wlogit.Models;
 
 namespace wlogit.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext db;
+        public HomeController(AppDbContext _db)
         {
-            return View();
+            db = _db;
         }
 
-        public IActionResult Post()
+
+        public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Post> myPost = db.Tbl_Post;
+            return View(myPost);
+        }
+
+
+        [Route("Home/Post/{Slug}")]
+        public IActionResult Post(string Slug)
+        {
+            var DetailedPost = db.Tbl_Post.Where(x=>x.Slug==Slug).FirstOrDefault();
+            return View(DetailedPost);
         }
 
 
