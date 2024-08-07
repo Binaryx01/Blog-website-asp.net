02b5319e1244dac7b689c950095a7c3dfb8acca3
Ghost <Ghost@BINARYX01>
2024-08-07 18:38:08 +0545

    Admin panel is added but still need to work on add post delete post and update post

---
+++ wlogit/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wlogit.Data;
+using wlogit.Models;
+
+namespace wlogit.Controllers
+{
+    public class AdminController : Controller
+    {
+
+
+        private readonly AppDbContext db;
+        public AdminController(AppDbContext _db)
+        {
+            db = _db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+
+
+
+
+
+        }
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(Post myPost)
+        {
+            db.Tbl_Post.Add(myPost);
+            db.SaveChanges();
+            return View();
+        }
+
+
+        public IActionResult AllPosts()
+        {
+            var myAllposts = db.Tbl_Post;
+            return View(myAllposts);
+        }
+
+    }
+}
