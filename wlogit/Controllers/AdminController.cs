e3037da994e8bc727e5d819c0de2d84433dc1483
Ghost <Ghost@BINARYX01>
2024-08-08 17:33:51 +0545

    Now we can upload image from admin panel

--- wlogit/Controllers/AdminController.cs
+++ wlogit/Controllers/AdminController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using wlogit.Data;
 using wlogit.Models;
+using wlogit.ViewModel;
 
 namespace wlogit.Controllers
 {
     public class AdminController : Controller
     {
 
+        IWebHostEnvironment env;
 
         private readonly AppDbContext db;
-        public AdminController(AppDbContext _db)
+        public AdminController(AppDbContext _db, IWebHostEnvironment environment)
         {
             db = _db;
+            env = environment;
         }
 
 
@@ -30,11 +33,34 @@ namespace wlogit.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddPost(Post myPost)
+        public IActionResult AddPost(PostVM myPost)
         {
-            db.Tbl_Post.Add(myPost);
-            db.SaveChanges();
+            if (ModelState.IsValid)
+
+            {
+                string ImageName = myPost.Image.FileName.ToString();
+                var FolderPath = Path.Combine(env.WebRootPath, "images");
+                var CompletePicPath = Path.Combine(FolderPath, ImageName);
+                myPost.Image.CopyTo(new FileStream(CompletePicPath, FileMode.Create));
+
+
+                Post post = new Post();
+                post.Title = myPost.Title;
+                post.SubTitle = myPost.SubTitle;
+                post.Date=myPost.Date;
+                post.Slug = myPost.Slug;
+                post.Content = myPost.Content;
+                post.Image = ImageName;
+                db.Tbl_Post.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index","Home");
+
+            
+            
+            }
+        
             return View();
+
         }
 
 
@@ -42,6 +68,42 @@ namespace wlogit.Controllers
         {
             var myAllposts = db.Tbl_Post;
             return View(myAllposts);
+        }
+
+
+        public IActionResult DeletePost(int Id)
+
+        {
+
+            var PostToDelete = db.Tbl_Post.Find(Id);
+
+            if (PostToDelete != null)
+            { 
+                db.Remove(PostToDelete);
+                db.SaveChanges();
+            
+            }
+
+            return RedirectToAction("AllPosts", "Admin");
+        
+        }
+
+        public IActionResult UpdatePost(int Id)
+        {
+            var PosttoUpdate = db.Tbl_Post.Find(Id);
+
+            return View(PosttoUpdate);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdatePost(Post post)
+        {
+            db.Tbl_Post.Update(post);
+            db.SaveChanges();
+            return RedirectToAction("AllPosts", "Admin");
+
+  
         }
 
     }
---
+++ wlogit/ViewModel/PostVM.cs
@@ -0,0 +1,17 @@
+﻿namespace wlogit.ViewModel
+{
+    public class PostVM
+    {
+
+
+      
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string? Content { get; set; }
+        public string? Date { get; set; }
+       // public string? Image { get; set; }
+        public IFormFile Image { get; set; }
+        
+        public string? Slug { get; set; }
+    }
+}
